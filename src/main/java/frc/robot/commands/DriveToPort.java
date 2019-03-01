04d68f721bcf1298a27ddcc707111b749462efae
Marvin <20linkjos@garpal.net>
2019-02-28 18:33:13 -0800

    Drive to Port

--- src/main/java/frc/robot/commands/DriveToPort.java
+++ src/main/java/frc/robot/commands/DriveToPort.java
@@ -8,8 +8,17 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.OI;
+import com.kauailabs.navx.frc.AHRS;
 
 public class DriveToPort extends Command {
+  Boolean HasRan = false;
+  public static AHRS ahrs;
+
+	double last_world_linear_accel_x;
+	double last_world_linear_accel_y;
   public DriveToPort() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
@@ -23,14 +32,29 @@ public class DriveToPort extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.m_Drivebase.HitTheTarget();
     //check for collision
+    		double curr_world_linear_accel_x = ahrs.getWorldLinearAccelX();
+        double currentJerkX = curr_world_linear_accel_x - last_world_linear_accel_x;
+        last_world_linear_accel_x = curr_world_linear_accel_x;
+        double curr_world_linear_accel_y = ahrs.getWorldLinearAccelY();
+        double currentJerkY = curr_world_linear_accel_y - last_world_linear_accel_y;
+        last_world_linear_accel_y = curr_world_linear_accel_y;
+        
+        if ( ( Math.abs(currentJerkX) > RobotMap.kCollisionThreshold_DeltaG ) ||
+             ( Math.abs(currentJerkY) > RobotMap.kCollisionThreshold_DeltaG) ) {
+            OI.DriverRumbleEnhanced(300, 1, true, true);
+            OI.OperatorRumbleEnhanced(300, 1, true, true);
+            HasRan=true;
+    }
+    
     
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return HasRan;
   }
 
   // Called once after isFinished returns true
