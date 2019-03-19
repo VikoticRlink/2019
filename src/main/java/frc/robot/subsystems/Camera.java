0ed86af0190c1b8b91ebae45f40a5a45da04ae9e
Marvin <20linkjos@garpal.net>
2019-03-19 16:30:27 -0700

    Tune camera

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -12,6 +12,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
 
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
@@ -30,34 +31,37 @@ public class Camera extends Subsystem {
     // setDefaultCommand(new MySpecialCommand());
   }
   public Camera(){
-    new Thread(() -> {
-      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-      //Set camera resolution.  640x480 may be too high, testing needed
-      camera.setResolution(640, 480);
-      
       //Steamworks example of putting overlay on the screen
       //https://www.programcreek.com/java-api-examples/index.php%3Fsource_dir=servdroid-master/android/ServDroid.web/src/org/servDroid/server/?code=Team2537/Cogsworth/Cogsworth-master/src/org/usfirst/frc/team2537/robot/cameras/Cameras.java
 
       
-      CvSink cvSink = CameraServer.getInstance().getVideo();
-      CvSource outputStream = CameraServer.getInstance().putVideo("Cargo", 640, 480);
-      
-      Mat source = new Mat();
-      Mat output = new Mat();
-      
-      while(!Thread.interrupted()) {
-        cvSink.grabFrame(source);
+      new Thread(() -> {
+        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+        camera.setResolution(320, 240);
+        
+        //CvSink cvSink = CameraServer.getInstance().getVideo();
+        //CvSource outputStream = CameraServer.getInstance().putVideo("Cargo", 320, 240);
+        
+        /*Mat source = new Mat();
+        Mat output = new Mat();
+        
+        while(!Thread.interrupted()) {
+            cvSink.grabFrame(source);
+            Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+            Imgproc.putText(source, "CARGO", new Point(output.cols() - 75, 25), 4, 0.8, new Scalar(255, 0, 0), 3);
+            Imgproc.line(source, new Point((output.cols()/2)-100, 0), new Point((output.cols()/2)-100, output.rows()), new Scalar(255, 0, 0), 1);
+            Imgproc.line(source, new Point((output.cols()/2)-99, 0), new Point((output.cols()/2)-100, output.rows()), new Scalar(255, 255, 0), 1);
+            Imgproc.line(source, new Point((output.cols()/2)+99, 0), new Point((output.cols()/2)+100, output.rows()), new Scalar(255, 255, 0), 1);
+            Imgproc.line(source, new Point((output.cols()/2)+100, 0), new Point((output.cols()/2)+100, output.rows()), new Scalar(255, 0, 0), 1);
+              
+            outputStream.putFrame(output);
+        }*/
 
-        Imgproc.putText(source, "CARGO", new Point(output.cols() - 75, 25), 4, 0.8, new Scalar(255, 0, 0), 3);
-        Imgproc.line(source, new Point((output.cols()/2)-100, 0), new Point((output.cols()/2)-100, output.rows()), new Scalar(255, 0, 0), 1);
-        Imgproc.line(source, new Point((output.cols()/2)-99, 0), new Point((output.cols()/2)-100, output.rows()), new Scalar(255, 255, 0), 1);
-        Imgproc.line(source, new Point((output.cols()/2)+99, 0), new Point((output.cols()/2)+100, output.rows()), new Scalar(255, 255, 0), 1);
-        Imgproc.line(source, new Point((output.cols()/2)+100, 0), new Point((output.cols()/2)+100, output.rows()), new Scalar(255, 0, 0), 1);
-           
-          //Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-          outputStream.putFrame(output);
-          
-      }
-  }).start();
+    }).start();
+  }
+  
+  @Override
+	public void periodic() {  
+	
   }
 }
